21bef8da5fddfcee92150f0ca3e94cd81ed86da2
Rayson Mendes <raysonmendes2.0@gmail.com>
2024-11-20 23:59:55 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function Home() {
+  return (
+    <div>
+      <h1>O primeiro desafio clone-tabnews</h1>
+      <p>
+        Sua Mente é para ter ideias, e não para guardá-las.
+      </p>
+      <p>
+        David Allen.
+      </p>
+    </div>
+      )
+}
+
+export default Home
