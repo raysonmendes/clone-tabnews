8063d94664147fc81055a72a571e82c395601931
Rayson Mendes <raysonmendes2.0@gmail.com>
2025-04-28 14:32:50 -0300

    refactor allow `GET` method

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,33 +1,43 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const updateAt = new Date().toISOString()
+  const allowedMethods = ["GET"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method '${request.method}' not allowed`,
+    });
+  }
 
-  const databaseVersionResult = await database.query("SHOW server_version;")
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version
-  
-  const databaseMaxConnetionsResult = await database.query("SHOW max_connections;")
-  const databaseMaxConnectionsValue = databaseMaxConnetionsResult.rows[0].max_connections
+  const updateAt = new Date().toISOString();
 
-  const databaseName = process.env.POSTGRES_DB
-  
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnetionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnetionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
   const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int  FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName]
-  })
-  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count
-  
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
-  response.status(200).json({ 
+  response.status(200).json({
     updated_at: updateAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
         opened_connections: databaseOpenedConnectionsValue,
-      }
-    }
-   });
+      },
+    },
+  });
 }
 
 export default status;
